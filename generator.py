d7da73c81458434f1fe293e69625eb2d409074c6
jas332211 <jas332211@wp.pl>
2020-07-29 12:54:27 +0200

    Update generator.py
    Added rotation & angles

--- generator.py
+++ generator.py
@@ -20,21 +20,37 @@ import cv2
 import imutils
 
 
-def single_square_data_generator(img_size = 64, # size of the img
-                                 padding = 10, # the number of MINIMUM of black spaces around or square - so that it doesnt get cut during rotation
+def single_square_data_generator(img_size = 91, # size of the img
+                                 min_square_size = 32,
+                                 max_square_size = 50, 
                                  own_sinogram_fnc = False,
-                                     ) -> " (sinogram ,image) tuple " :
+                                 rotation = False,
+                                     ) -> " (sinogram , image, angles) tuple ":
+    
+    
+    padding = (img_size - max_square_size)//2
 
 
     img = np.zeros((img_size,img_size)) # create an empty image
-    square_size = randint(3+padding,img_size-3-padding) # generate a random size of a square side
-    x = randint(1,64-square_size-padding) # left starting point
-    y = randint(1,64-square_size-padding) # up starting point
+    square_size = randint(min_square_size, max_square_size) # generate a random size of a square side
+    x = randint(padding//1.5,padding) # left starting point
+    y = randint(padding//1.5,padding) # up starting point
     
     # We got starting point and size of square, now, draw it ! :
     for i in range(square_size) : 
         for j in range(square_size) : 
             img[x+i,y+j] = 1 
+            
+            
+    #Generate the best angles : 
+    angles = np.array([0, 90])
+            
+            
+    # If we want our data to be rotated :
+    if rotation : 
+        rotation_angle = randint(0,90)
+        img = imutils.rotate(img, rotation_angle)
+        angles+= rotation_angle
             
     
     #Generate sinogram, either with the library or from own method :
@@ -43,28 +59,39 @@ def single_square_data_generator(img_size = 64, # size of the img
     else : 
         sinogram = radon(img) 
         
-    return (sinogram, img)
+        
+    
+        
+    return (sinogram, img, angles)
 
 
-def square_data_generator(n : "number of data to be generated",
-                          img_size : "size of the square's side",
-                          ) -> " (x,y) where x - list of sinograms, y - list of corresponding images" :
 
-    x, y = [], []    
+
+def square_data_generator(n = 1,  #number of data to be generated
+                          img_size = 91,  # size of the image 
+                          min_square_size = 32,
+                          max_square_size = 50, 
+                          own_sinogram_fnc = False,  # Which sinogram generator to use ? 
+                          rotation = False, # Rotate the image ? 
+                          ) -> " (sinogram, image, [angles] ) " :
+
+    x, y, z = [], [], []    
     for _ in range(n):
-        sinogram, img = single_square_data_generator(img_size)
+        sinogram, img, angles = single_square_data_generator(img_size, min_square_size, max_square_size, own_sinogram_fnc, rotation)
         x.append(sinogram)
         y.append(img)
+        z.append(angles)
         
     x = np.array(x)
     y = np.array(y)
+    z = np.array(z)
     
     #### To make it fit the NN we need to transform these to tensors, change their dimensions, and cast into double [float32]
     x = torch.from_numpy(x)
-    x = x.view(-1,1,64,180)
+    x = x.view(-1,1,91,180)
     x = x.to(dtype=torch.float32)
     
-    return (x,y)
+    return (x, y, z)
 
 
 
